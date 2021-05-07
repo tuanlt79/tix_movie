f181fa9f1d5d9ea639ca94cf92715bb35310d649
Digilife <tuanlt79@outlook.com.vn>
2021-05-07 08:59:06 +0700

    checkout_v2

--- src/Component/Checkout/Checkout.jsx
+++ src/Component/Checkout/Checkout.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { Fragment } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { layThongTinPhongVeAction } from "../../Action/PhimAction";
 
@@ -10,9 +11,39 @@ export default function Checkout(props) {
     dispatch(layThongTinPhongVeAction(id));
   }, []);
   console.log({ thongTinPhongVe });
+  let renderGhe = () => {
+    return thongTinPhongVe.danhSachGhe?.map((ghe, index) => {
+      return (
+        <Fragment key={index}>
+          <button>{ghe.stt}</button>
+          {(index + 1) % 12 === 0 ? <br /> : ""}
+        </Fragment>
+      );
+    });
+  };
   return (
     <section className="checkout">
-      <div></div>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="seat__map">
+            <div className="col-lg-9">
+              <div className="step__checkout">
+                <span> CHỌN GHẾ & THANH TOÁN</span>
+              </div>
+              <div className="seat__checkout">
+                <div className="screen">
+                  <img
+                    src="https://tix.vn/app/assets/img/icons/screen.png"
+                    alt=""
+                  />
+                </div>
+                <div className="listseat">{renderGhe()}</div>
+              </div>
+            </div>
+            <div className="col-lg-3"></div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
